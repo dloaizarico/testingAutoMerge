ada73c0ab13ef7b3c46c050617085643031cc2dc
Diego Loaiza <diego@bestperformance.com.au>
2023-02-17 20:55:54 +0800

    first hotfix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Changing this as a hotfix.
         </a>
       </header>
     </div>
